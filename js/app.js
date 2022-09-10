22d7621dea48bb8702187c2cbb4001fe8b9eee02
mohammadammash <mohammadammash31@gmail.com>
2022-09-10 09:02:38 +0300

    Adding EventListener for the submit button in 'js/app.js' to handle the click and validate the input

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const gender = document.getElementById("gender");
 const age = document.getElementById("age");
 const nationalities = document.getElementById("nationalities");
 
-// we fetch from an API a dog image then show it, and we handle any error by showing a local dog image instead.
+// We fetch from an API a dog image then show it, and we handle any error by showing a local dog image instead.
 const fetch_dog_image = async () => {
   try {
     const response = await fetch("https://dog.ceo/api/breeds/image/random");
@@ -18,5 +18,20 @@ const fetch_dog_image = async () => {
   }
 };
 
+const generatePredictions = () => {
+  // Get the inputValue(name) and validate first if it contains special characters or numbers
+  const inputName = input.value;
+  let lettersValidation = /^[A-Za-z]+$/.test(inputName);
+  if (!inputName || !lettersValidation) {
+    predictions.classList.add("display-none");
+    return;
+  }
+  // If input passed the validation we display the paragraph that will contain the results
+  if (predictions.classList.contains("display-none")) {
+    predictions.classList.remove("display-none");
+  }
+};
 // Each time our window loads
 window.addEventListener("load", fetch_dog_image);
+// Each time the Guess button clicked => generatePredictions()
+submitBtn.addEventListener("click", generatePredictions);
