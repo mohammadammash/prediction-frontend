76509a6df409f5de777fa1586e6df619fc934a6e
mohammadammash <mohammadammash31@gmail.com>
2022-09-10 08:59:03 +0300

    Fetching dog_image_API in 'js/app.js' each time our window loads and show it in 'index.html'

--- js/app.js
+++ js/app.js
@@ -5,3 +5,18 @@ const predictions = document.getElementById("predictions");
 const gender = document.getElementById("gender");
 const age = document.getElementById("age");
 const nationalities = document.getElementById("nationalities");
+
+// we fetch from an API a dog image then show it, and we handle any error by showing a local dog image instead.
+const fetch_dog_image = async () => {
+  try {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = await response.json();
+    if (!data.message) throw "empty";
+    img.src = data.message;
+  } catch {
+    img.src = "../styles/images/dogs.jfif";
+  }
+};
+
+// Each time our window loads
+window.addEventListener("load", fetch_dog_image);
