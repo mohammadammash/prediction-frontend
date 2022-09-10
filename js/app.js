2dc974ec67795ad76fe8b2a16f4c34e3df3a7f12
mohammadammash <mohammadammash31@gmail.com>
2022-09-10 08:53:40 +0300

    Linking 'index.html' to 'js/app.js' and intializing constants in 'js/app.js'

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const submitBtn = document.getElementById("submit-btn");
+const img = document.getElementById("dog-image");
+const input = document.getElementById("name");
+const predictions = document.getElementById("predictions");
+const gender = document.getElementById("gender");
+const age = document.getElementById("age");
+const nationalities = document.getElementById("nationalities");
